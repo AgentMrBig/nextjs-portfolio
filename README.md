36f24c7e3a69b6176f2d48b84df5db40111ede8a
Eric Hendrix <agentmrbig@gmail.com>
2019-08-11 20:34:00 -0400

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # nextjs-portfolio
+
+Personal portfolio project to display my software dev / web dev knowledge
---
+++ pages/index.js
@@ -0,0 +1,5 @@
+import Link from 'next/link';
+
+export default () => (
+    <p>Hello Next What <Link href="/about"><a>Learn more</a></Link></p>
+)
